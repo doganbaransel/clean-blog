c77e6466ad4bd1b714b508e7ad6f66d2e08b95e9
doganbaransel <doganbaransel@gmail.com>
2024-02-02 12:15:40 +0300

    Some changes

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+  const blog = { id: 1, title: "Blog title", description: "Blog description" };
+  res.send(blog);
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server started on ${port} port.`);
+});
